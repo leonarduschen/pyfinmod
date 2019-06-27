8bc2158fd4c991208ac5f5e11ab48be468a6c88a
Alexey Smirnov <alexey.smirnov@customergauge.com>
2019-06-27 15:52:32 +0200

    add net debt to enterprise valuation

--- pyfinmod/wacc.py
+++ pyfinmod/wacc.py
@@ -7,7 +7,22 @@ def _company_debt(column):
     long_term_debt = column['Long Term Debt']
     short_long_debt = column['Short/Current Long Term Debt']
     cash = column['Cash And Cash Equivalents']
+
     return long_term_debt + short_long_debt - cash
+
+
+def _company_net_debt(column):
+    """
+    Uses balance sheet to get company net debt. Works only with YahooFinanceParser
+    :param column: Yahoo Finance page parsed to dataframe with pyfinmod.yahoo_finance.YahooFinanceParser
+    :return: series of debt values by date
+    """
+    long_term_debt = column['Long Term Debt']
+    short_long_debt = column['Short/Current Long Term Debt']
+    cash = column['Cash And Cash Equivalents']
+    short_investments = column['Short Term Investments']
+
+    return long_term_debt + short_long_debt - cash - short_investments
 
 
 def _company_tax_rate(column):
@@ -21,9 +36,13 @@ def _company_tax_rate(column):
     return income_tax_expense / income_before_tax
 
 
-def get_debt(dataframe):
-    return dataframe.apply(_company_debt)
+def get_debt(balance_sheet_dataframe):
+    return balance_sheet_dataframe.apply(_company_debt)
 
 
-def get_tax_rate(dataframe):
-    return dataframe.apply(_company_tax_rate)
+def get_tax_rate(income_statement_dataframe):
+    return income_statement_dataframe.apply(_company_tax_rate)
+
+
+def get_cost_of_debt(balance_sheet_dataframe, income_statement_dataframe):
+    return balance_sheet_dataframe.apply(_company_net_debt)
