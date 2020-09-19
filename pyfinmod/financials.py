cb45d878c961be5a8ab83f858de2e6847f484a47
leonarduschen <leonardus.chen@gmail.com>
2020-09-19 19:54:52 +0800

    Change datatypes container

--- pyfinmod/financials.py
+++ pyfinmod/financials.py
@@ -9,13 +9,20 @@ class ParserError(Exception):
 
 
 class Financials:
-    url_financials = "https://financialmodelingprep.com/api/v3/financials/{}/{}?apikey=demo"
-    url_profile = "https://financialmodelingprep.com/api/v3/company/profile/{}?apikey=demo"
-    available_data_type = (
-        "balance_sheet_statement",
-        "cash_flow_statement",
-        "income_statement",
-    )
+    """Financial data parser
+
+    Parameters:
+    ticker : str
+        Public company ticker to fetch the financial data of (e.g. 'AAPL')
+
+    """
+    base_url = "https://financialmodelingprep.com/api/v3/"
+    datatypes = {
+        "balance_sheet_statement": base_url + "financials/balance-sheet-statement/{}?apikey=demo",
+        "cash_flow_statement": base_url + "financials/cash-flow-statement/{}?apikey=demo",
+        "income_statement": base_url + "financials/income-statement/{}?apikey=demo",
+        "profile": base_url + "company/profile/{}?apikey=demo"
+    }
 
     def __init__(self, ticker):
         self.ticker = ticker
@@ -28,24 +35,33 @@ class Financials:
     def _date_parse(date_str):
         return datetime.strptime(date_str, "%Y-%m-%d").date()
 
-    def _fetch_json(self, data_type):
+    def _fetch_json(self, datatype):
         try:
-            if data_type != "profile":
-                url = self.url_financials.format(
-                    data_type.replace("_", "-"), self.ticker
-                )
-            else:
-                url = self.url_profile.format(self.ticker)
+            url = self.datatypes[datatype].format(self.ticker)
             res = requests.get(url, timeout=5)
         except requests.exceptions.RequestException as e:
-            raise ParserError("Failed to get data from external api {}".format(e))
-        json = res.json()
-        if not json:
-            raise ParserError("empty response from api")
-        return json
+            raise ParserError("Failed to get data from external API {}".format(e))
+        else:
+            json = res.json()
+            if not json:
+                raise ParserError("Empty response from external API")
+            return json
+
+    def _json_to_df(self, json):
+        _r = defaultdict(list)
+        keys = [i for i in json[0].keys() if i != "date"]
+        _r["row name"] = keys
+        for row in json:
+            _r[self._date_parse(row["date"])] = [
+                float(v) for k, v in row.items() if k in keys
+            ]
+
+        df = pd.DataFrame.from_dict(_r)
+        df = df.set_index("row name")
+        return df
 
     def __getattr__(self, item):
-        if item in self.available_data_type:
+        if item in self.datatypes:
             cached_value_key = "_" + item
             cached_value = getattr(self, cached_value_key, None)
             if cached_value:
@@ -64,16 +80,3 @@ class Financials:
                 json_data = self._fetch_json("profile")["profile"]
                 setattr(self, cached_value_key, json_data)
                 return float(json_data.get(item, 0))
-
-    def _json_to_df(self, json):
-        _r = defaultdict(list)
-        keys = [i for i in json[0].keys() if i != "date"]
-        _r["row name"] = keys
-        for row in json:
-            _r[self._date_parse(row["date"])] = [
-                float(v) for k, v in row.items() if k in keys
-            ]
-
-        df = pd.DataFrame.from_dict(_r)
-        df = df.set_index("row name")
-        return df
