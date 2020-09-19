bd91967ca6f24e7176668e5f3f8453e38adb2a35
leonarduschen <leonardus.chen@gmail.com>
2020-09-19 21:23:45 +0800

    Add additional tests and cleanup

--- tests/test_financials_parser.py
+++ tests/test_financials_parser.py
@@ -11,6 +11,15 @@ def test_date_parser():
     assert Financials._date_parse("2018-9-29") == date(2018, 9, 29)
 
 
+def test_json_to_df():
+    with open(os.path.join(raw_data_dir, "aapl_balance_sheet.json"), "r") as f:
+        json_data = json.load(f)
+
+    results = Financials._json_to_df(json_data)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
+    assert results.equals(expected)
+
+
 def test_get_balance_sheet():
     parser = Financials("AAPL")
     with open(os.path.join(raw_data_dir, "aapl_balance_sheet.json"), "r") as f:
@@ -18,11 +27,11 @@ def test_get_balance_sheet():
 
     parser._fetch_json = lambda x: {"financials": json_data}
 
-    df = parser.balance_sheet_statement
-    assert not df.empty
+    results = parser.balance_sheet_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
+    assert results.equals(expected)
 
 
 def test_get_income_statement():
@@ -31,11 +40,11 @@ def test_get_income_statement():
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
 
-    df = parser.income_statement
-    assert not df.empty
+    results = parser.income_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_income_statement.hdf"), key="aapl_income_statement")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_income_statement.hdf"), key="aapl_income_statement")
+    assert results.equals(expected)
 
 
 def test_get_cash_flow():
@@ -44,16 +53,15 @@ def test_get_cash_flow():
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
 
-    df = parser.cash_flow_statement
-    assert not df.empty
+    results = parser.cash_flow_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_cash_flow.hdf"), key="aapl_cash_flow")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_cash_flow.hdf"), key="aapl_cash_flow")
+    assert results.equals(expected)
 
 
 def test_get_market_cap():
     parser = Financials("AAPL")
-    parser._fetch_json("profile")
     with open(os.path.join(raw_data_dir, "aapl_summary.json"), "r") as f:
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
