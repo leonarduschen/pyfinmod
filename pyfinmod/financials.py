00381012d5f7332a277d7f4f5c184fdbb99b4ac8
Leonardus Chen <leonardus.chen@gmail.com>
2020-09-19 22:15:01 +0800

    Refactor financial parser (#11)
    - Change class variable `Financials.data_types` to act as URL container
    - Change `Financials.__getattr__` flow controls to be more explicit
    - Make `Financials.json_to_df` a static method
    - Add additional tests and reworked existing ones
    - Add docstrings and code cleanups

--- pyfinmod/financials.py
+++ pyfinmod/financials.py
@@ -9,13 +9,20 @@ class ParserError(Exception):
 
 
 class Financials:
-    url_financials = "https://financialmodelingprep.com/api/v3/financials/{}/{}?apikey=demo"
-    url_profile = "https://financialmodelingprep.com/api/v3/company/profile/{}?apikey=demo"
-    available_data_type = (
-        "balance_sheet_statement",
-        "cash_flow_statement",
-        "income_statement",
-    )
+    """Financial data parser
+
+    Parameters:
+    ticker : str
+        Public company ticker to fetch the financial data of (e.g. 'AAPL')
+
+    """
+    base_url = "https://financialmodelingprep.com/api/v3/"
+    datatypes = {
+        "balance_sheet_statement": base_url + "financials/balance-sheet-statement/{}?apikey=demo",
+        "cash_flow_statement": base_url + "financials/cash-flow-statement/{}?apikey=demo",
+        "income_statement": base_url + "financials/income-statement/{}?apikey=demo",
+        "profile": base_url + "company/profile/{}?apikey=demo"
+    }
 
     def __init__(self, ticker):
         self.ticker = ticker
@@ -26,54 +33,71 @@ class Financials:
 
     @staticmethod
     def _date_parse(date_str):
+        """Parse dates and then convert from datetime.datetime to datetime.date
+
+        """
         return datetime.strptime(date_str, "%Y-%m-%d").date()
 
-    def _fetch_json(self, data_type):
-        try:
-            if data_type != "profile":
-                url = self.url_financials.format(
-                    data_type.replace("_", "-"), self.ticker
-                )
-            else:
-                url = self.url_profile.format(self.ticker)
-            res = requests.get(url, timeout=5)
-        except requests.exceptions.RequestException as e:
-            raise ParserError("Failed to get data from external api {}".format(e))
-        json = res.json()
-        if not json:
-            raise ParserError("empty response from api")
-        return json
+    @staticmethod
+    def _json_to_df(json):
+        """Convert JSON to pd.DataFrame
 
-    def __getattr__(self, item):
-        if item in self.available_data_type:
-            cached_value_key = "_" + item
-            cached_value = getattr(self, cached_value_key, None)
-            if cached_value:
-                return cached_value
-            else:
-                json_data = self._fetch_json(item)["financials"]
-                df = self._json_to_df(json_data)
-                setattr(self, cached_value_key, df)
-                return df
-        else:
-            cached_value_key = "_profile"
-            cached_value = getattr(self, cached_value_key, None)
-            if cached_value:
-                return float(cached_value.get(item))
-            else:
-                json_data = self._fetch_json("profile")["profile"]
-                setattr(self, cached_value_key, json_data)
-                return float(json_data.get(item, 0))
+        To be used for balance_sheet_statement, cash_flow_statement, and income_statement only
 
-    def _json_to_df(self, json):
+        """
         _r = defaultdict(list)
         keys = [i for i in json[0].keys() if i != "date"]
-        _r["row name"] = keys
+        _r["Items"] = keys
         for row in json:
-            _r[self._date_parse(row["date"])] = [
+            _r[Financials._date_parse(row["date"])] = [
                 float(v) for k, v in row.items() if k in keys
             ]
 
         df = pd.DataFrame.from_dict(_r)
-        df = df.set_index("row name")
+        df = df.set_index("Items")
         return df
+
+    def _fetch_json(self, datatype):
+        """Fetch the requested datatype from the corresponding URL provided in self.datatype class variable
+
+        """
+        try:
+            url = self.datatypes[datatype].format(self.ticker)
+            res = requests.get(url, timeout=5)
+        except requests.exceptions.RequestException as e:
+            raise ParserError("Failed to get data from external API {}".format(e))
+        else:
+            json = res.json()
+            if not json:
+                raise ParserError("Empty response from external API")
+            return json
+
+    def __getattr__(self, name):
+        """Return financial data stored as attribute
+
+
+        If balance sheet, cash flow statement, or income statement is requested then return a pd.DataFrame.
+        If profile is requested then return a dictionary (JSON format).
+        Otherwise, attempt to search self.profile for the requested data and return the corresponding value if found.
+
+        """
+        if name in ["balance_sheet_statement", "cash_flow_statement", "income_statement"]:
+            cached_value = getattr(self, "_" + name, None)
+            if cached_value is not None:
+                return cached_value
+            else:
+                json_data = self._fetch_json(name)["financials"]
+                df = self._json_to_df(json_data)
+                setattr(self, "_" + name, df)
+                return df
+        elif name in ["profile"]:
+            cached_value = getattr(self, "_" + name, None)
+            if cached_value is not None:
+                return cached_value
+            else:
+                json_data = self._fetch_json(name)["profile"]
+                setattr(self, "_" + name, json_data)
+                return json_data
+        else:
+            json_data = self.profile
+            return float(json_data.get(name, 0))
--- tests/test_financials_parser.py
+++ tests/test_financials_parser.py
@@ -1,14 +1,37 @@
 import os
 import json
+from json import JSONDecodeError
 from datetime import date
 import pandas as pd
-from pyfinmod.financials import Financials
+import pytest
+from pyfinmod.financials import Financials, ParserError
 
 raw_data_dir = os.path.join(os.path.dirname(__file__), 'raw_data')
 
 
 def test_date_parser():
     assert Financials._date_parse("2018-9-29") == date(2018, 9, 29)
+
+
+def test_json_to_df():
+    with open(os.path.join(raw_data_dir, "aapl_balance_sheet.json"), "r") as f:
+        json_data = json.load(f)
+
+    results = Financials._json_to_df(json_data)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
+    assert results.equals(expected)
+
+
+def test_fetch_json():
+    parser = Financials("AAPL")
+    parser.datatypes["balance_sheet_statement"] = "NotaValidURL"
+    parser.datatypes["cash_flow_statement"] = "https://www.google.com"
+ 
+    # Wrong url should raise ParseError
+    with pytest.raises(ParserError):
+        parser._fetch_json("balance_sheet_statement")
+    with pytest.raises(JSONDecodeError):
+        parser._fetch_json("cash_flow_statement")
 
 
 def test_get_balance_sheet():
@@ -18,11 +41,16 @@ def test_get_balance_sheet():
 
     parser._fetch_json = lambda x: {"financials": json_data}
 
-    df = parser.balance_sheet_statement
-    assert not df.empty
+    results = parser.balance_sheet_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_balance_sheet.hdf"), key="aapl_balance_sheet")
+    assert results.equals(expected)
+
+    # test cached value
+    parser._fetch_json = None
+    results = parser.balance_sheet_statement
+    assert results.equals(expected)
 
 
 def test_get_income_statement():
@@ -31,11 +59,16 @@ def test_get_income_statement():
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
 
-    df = parser.income_statement
-    assert not df.empty
+    results = parser.income_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_income_statement.hdf"), key="aapl_income_statement")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_income_statement.hdf"), key="aapl_income_statement")
+    assert results.equals(expected)
+
+    # test cached value
+    parser._fetch_json = None
+    results = parser.income_statement
+    assert results.equals(expected)
 
 
 def test_get_cash_flow():
@@ -44,18 +77,27 @@ def test_get_cash_flow():
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
 
-    df = parser.cash_flow_statement
-    assert not df.empty
+    results = parser.cash_flow_statement
+    assert not results.empty
 
-    df_test = pd.read_hdf(os.path.join(raw_data_dir, "aapl_cash_flow.hdf"), key="aapl_cash_flow")
-    assert df.equals(df_test)
+    expected = pd.read_hdf(os.path.join(raw_data_dir, "aapl_cash_flow.hdf"), key="aapl_cash_flow")
+    assert results.equals(expected)
+
+    # test cached value
+    parser._fetch_json = None
+    results = parser.cash_flow_statement
+    assert results.equals(expected)
 
 
 def test_get_market_cap():
     parser = Financials("AAPL")
-    parser._fetch_json("profile")
     with open(os.path.join(raw_data_dir, "aapl_summary.json"), "r") as f:
         json_data = json.load(f)
     parser._fetch_json = lambda x: json_data
-    mktCap = parser.mktCap
-    assert mktCap == float(1230468047640.00)
+    results = parser.mktCap
+    assert results == float(1230468047640.00)
+
+    # test cached value
+    parser._fetch_json = None
+    results = parser.mktCap
+    assert results == float(1230468047640.00)
